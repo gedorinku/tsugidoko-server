b628b97592367d62f24fe367614002410f1909c2
Ryota Egusa <gedorinku@yahoo.co.jp>
2018-12-30 17:01:17 +0900

    Impl ClassRoomStore

--- store/class_room/class_room_store.go
+++ store/class_room/class_room_store.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/pkg/errors"
+	"github.com/volatiletech/sqlboiler/queries/qm"
+
 	"github.com/gedorinku/tsugidoko-server/infra/record"
 	"github.com/gedorinku/tsugidoko-server/store"
 )
@@ -22,9 +25,34 @@ func NewClassRoomStore(ctx context.Context, db *sql.DB) store.ClassRoomStore {
 }
 
 func (s *classRoomStoreImpl) ListClassRoom() ([]*record.ClassRoom, error) {
-	panic("not implemented")
+	mods := []qm.QueryMod{
+		qm.Load(record.ClassRoomRels.Beacons),
+		qm.Load(record.ClassRoomRels.ClassRoomTags),
+	}
+	res, err := record.ClassRooms(mods...).All(s.ctx, s.db)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, errors.WithStack(err)
+	}
+
+	return res, nil
 }
 
 func (s *classRoomStoreImpl) GetClassRoom(classRoomID int64) (*record.ClassRoom, error) {
-	panic("not implemented")
+	mods := []qm.QueryMod{
+		qm.Load(record.ClassRoomRels.Beacons),
+		qm.Load(record.ClassRoomRels.ClassRoomTags),
+		qm.Where("id = ?", classRoomID),
+	}
+	res, err := record.ClassRooms(mods...).One(s.ctx, s.db)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, err
+		}
+		return nil, errors.WithStack(err)
+	}
+
+	return res, nil
 }
