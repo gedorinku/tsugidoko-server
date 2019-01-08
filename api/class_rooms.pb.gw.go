2c5fc6bc01e141b5c6998aa1af14c20505b764b6
Ryota Egusa <gedorinku@yahoo.co.jp>
2019-01-08 14:53:57 +0900

    Update proto

--- api/class_rooms.pb.gw.go
+++ api/class_rooms.pb.gw.go
@@ -28,9 +28,17 @@ var _ status.Status
 var _ = runtime.String
 var _ = utilities.NewDoubleArray
 
+var (
+	filter_ClassRoomService_ListClassRooms_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
 func request_ClassRoomService_ListClassRooms_0(ctx context.Context, marshaler runtime.Marshaler, client ClassRoomServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq ListClassRoomsRequest
 	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_ClassRoomService_ListClassRooms_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
 
 	msg, err := client.ListClassRooms(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
